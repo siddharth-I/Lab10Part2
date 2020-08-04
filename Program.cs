c2673c7374715d5e9892ee02544d8aabb07b74ac
siddharth-I <67158060+siddharth-I@users.noreply.github.com>
2020-08-03 22:14:22 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Circle
+{
+    class Program
+    {
+        public double Radius { get; set; }
+        public double Circumference { get; set; }
+
+        public double Area { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+        public double CalculateCircumference()
+        {
+            double Circumference = 2 * Math.PI * Radius;
+        }
+
+        public double CalculateFormattedCircumference()
+        {
+            Math.Round(Circumference, 2);
+        }
+
+        public double CalculateArea()
+        {
+            double Area = Math.PI * (Radius * Radius);
+        }
+
+        public double CalculateFormattedArea()
+        {
+            Math.Round(Area, 2);
+        }
+
+        public double FormatNumber(double x)
+        {
+            //???
+        }
+    }
+}
